20648debb0ae9a71106537e217045637be630b85
krishnakanthati <krishnakanthati@outlook.com>
2021-05-04 15:08:30 +0530

    whatsappme

--- automate.py
+++ automate.py
@@ -1,15 +1,17 @@
+import os
 from twilio.rest import Client
 
 account_sid = 'ACd5b0197532b5170656ff145fe4f411d0'
-auth_token = '567a9a0caf5b348f60ad8215fa4da43f'
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+my_whatsapp = os.environ.get('TWILIO_MY_WHATSAPP_NUMBER')
+
 client = Client(account_sid, auth_token)
 
 
 def send():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
-        body='Krishna Kant Hati\nCoder\nwww.superlikes.herokuapp.com',
-        to='whatsapp:+918217243741'
+        body='Krishna Kant Hati',
+        to='whatsapp:' + my_whatsapp
     )
-
     print(message.sid)
