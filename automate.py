1dcfa1f8d1196a29da6757caff16a950e29baa5a
krishnakanthati <krishnakanthati@outlook.com>
2021-05-04 15:23:08 +0530

    whatsappme

--- automate.py
+++ automate.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 
 account_sid = 'ACd5b0197532b5170656ff145fe4f411d0'
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-my_whatsapp = os.environ.get('TWILIO_MY_WHATSAPP_NUMBER')
+my_number = os.environ.get('TWILIO_MY_WHATSAPP_NUMBER')
 
 client = Client(account_sid, auth_token)
 
@@ -12,6 +12,6 @@ def send():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body='Krishna Kant Hati ✔️\nR⭕LLACUBE ❤️',
-        to='whatsapp:' + my_whatsapp
+        to='whatsapp:' + my_number
     )
     print(message.sid)
