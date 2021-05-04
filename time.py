7273196d92f825fc47f2725b30ca610aa40343d3
krishnakanthati <krishnakanthati@outlook.com>
2021-05-04 15:17:33 +0530

    whatsappme

--- time.py
+++ time.py
@@ -4,6 +4,6 @@ from automate import send
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(send, 'interval', seconds=10)
+sched.add_job(send, 'interval', minutes=15)
 
 sched.start()
